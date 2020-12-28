c3fd835e8df4912c6ce80c9078a50ba41bade346
Kay Steinbauer <kst@outermedia.de>
2020-12-29 00:33:24 +0100

    adds route for single painting

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ router.get("/", (req, res) => {
     res.render("home");
 });
 
+// @desc Dashboard
+// @route GET /dashboard
 router.get("/dashboard", async (req, res) => {
     const collection = db.collection('paintings');
     const snapshot = await collection.get();
@@ -18,7 +20,7 @@ router.get("/dashboard", async (req, res) => {
         data.id = doc.id;
         paintings.push(data);
     });
-    console.log(paintings);
+    console.log(req.params);
 
     res.render("dashboard", {
         paintings: paintings
@@ -27,5 +29,27 @@ router.get("/dashboard", async (req, res) => {
 router.get("/paintings", (req, res) => {
     res.render("paintings")
 })
+
+// @desc Show user picture
+// @route GET /
+router.get("/painting/:id", async (req, res) => {
+    const pictureId = req.params.id;
+
+    // Load the picture to id
+    const collection = db.collection('paintings');
+    await collection.doc(pictureId)
+        .get()
+        .then((doc) => {
+            let painting = doc.data();
+            painting.id = doc.id;
+            res.render("paintings/show-painting", {
+                painting: painting
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
 
 module.exports = router;
