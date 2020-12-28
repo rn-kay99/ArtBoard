cdc13cd07ea9e5298ff8218a9ed8a893389f5ee4
Kay Steinbauer <kst@outermedia.de>
2020-12-28 22:18:51 +0100

    adds new routes

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,14 @@ router.get("/", (req, res) => {
     res.render("home");
 });
 
+router.get("/dashboard", (req, res) => {
+    let name = "kay";
+    res.render("dashboard", {
+        name: name
+    });
+})
+router.get("/paintings", (req, res) => {
+    res.render("paintings")
+})
+
 module.exports = router;
