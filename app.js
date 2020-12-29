e276e2999c081333d95454d23afdc369edf93493
Kay Steinbauer <kst@outermedia.de>
2020-12-29 21:25:00 +0100

    adds formatDate helper

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Handlebars Helper
+const { formatDate } = require("./helpers/hbs")
+
 // Handlebars
-app.engine("hbs", handlebars({extname: "hbs"}));
+app.engine("hbs", handlebars({ helpers: {formatDate}, extname: "hbs" }));
 app.set('view engine', 'hbs');
 
 
